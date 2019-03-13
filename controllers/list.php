296b2837cc43d5cee3ef83f21d1206fff53a116b
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-03-13 12:04:15 -0400

    full crud for list model achieved

--- controllers/list.php
+++ controllers/list.php
@@ -1,9 +1,30 @@
 <?php
+
   include_once __DIR__ . '/../models/list_item.php';
   header('Content-Type: application/json');
 
   if($_REQUEST['action'] === 'index'){
     echo json_encode(ListItems::all());
+
+  } else if ($_REQUEST['action'] === 'post'){
+      $request_body = file_get_contents('php://input');
+      $body_object = json_decode($request_body);
+      $new_listItem = new ListItem(null, $body_object->item, $body_object->category, $body_object->price, $body_object->quantity, $body_object->unit, $body_object->in_refrigerator);
+      $all_list_items = ListItems::create($new_listItem);
+      echo json_encode($all_list_items);
+
+  } else if ($_REQUEST['action'] === 'update'){
+    $request_body = file_get_contents('php://input');
+    $body_object = json_decode($request_body);
+    $updated_item = new ListItem($_REQUEST['id'], $body_object->item, $body_object->category, $body_object->price, $body_object->quantity, $body_object->unit, $body_object->in_refrigerator);
+
+    $all_list_items = ListItems::update($updated_item);
+
+    echo json_encode($all_list_items);
+
+  } else if ($_REQUEST['action'] === 'delete'){
+    $all_list_items = ListItems::delete($_REQUEST['id']);
+    echo json_encode($all_list_items);
   }
 
 
--- models/list_item.php
+++ models/list_item.php
@@ -1,17 +1,36 @@
 <?php
-  $host = getenv('HOST')
-  $dbconn = pg_connect("host=$host dbname=shopping_list");
+  // require 'vendor/autoload.php';
+  // require 'vendor/vlucas/phpdotenv/src/Dotenv.php';
+  // require 'vendor/vlucas/phpdotenv/src/Loader.php';
+  // require 'vendor/vlucas/phpdotenv/src/Validator.php';
+  // $dotenv = Dotenv\Dotenv::create(__DIR__);
+  // $dotenv->load();
+  // $host = getenv('HOST');
+
+  // function pg_connection_string(){
+  //   return "dbname=dl5fbvdq9tl02 host=ec2-50-19-109-120.compute-1.amazonaws.com port=5432 user=dkhhvwyafmoelq password=537dee3d8fc128e53a14daff804e51bbfb3e0612a179d012286a1c4a35a1950a sslmode=require"
+  // };
+  // $dbconn = pg_connect(pg_connection_string());
+
+  $dbconn = pg_connect("host=localhost dbname=shopping_list");
+
 
   class ListItem {
     public $id;
-    public $produce;
-    public $meat;
-    public $grocery;
-    public function __construct($id, $produce, $meat, $grocery){
+    public $item;
+    public $category;
+    public $price;
+    public $quantity;
+    public $unit;
+    public $in_refrigerator;
+    public function __construct($id, $item, $category, $price, $quantity, $unit, $in_refrigerator){
       $this->id = $id;
-      $this->produce = $produce;
-      $this->meat = $meat;
-      $this->grocery = $grocery;
+      $this->item = $item;
+      $this->category = $category;
+      $this->price = $price;
+      $this->quantity = $quantity;
+      $this->unit = $unit;
+      $this->in_refrigerator = $in_refrigerator;
     }
   }
 
@@ -24,21 +43,45 @@
       $row_object = pg_fetch_object($results);
       while($row_object){
         $new_listItem = new ListItem(
-            $row_object->id,
-            $row_object->produce,
-            $row_object->meat,
-            $row_object->grocery
+            intval($row_object->id),
+            $row_object->item,
+            $row_object->category,
+            $row_object->price,
+            intval($row_object->quantity),
+            $row_object->unit,
+            $row_object->in_refrigerator
           );
           $listItems[] = $new_listItem;
-
           $row_object = pg_fetch_object($results);
       }
-
       return $listItems;
-
-
-
     }
+
+    static function create($newItem){
+      $query = "INSERT INTO list (item, category, price, quantity, unit) VALUES ($1, $2, $3, $4, $5)";
+      $query_params = array($newItem->item, $newItem->category, $newItem->price, $newItem->quantity, $newItem->unit);
+      pg_query_params($query, $query_params);
+      return self::all();
+    }
+
+    static function update($updated_item){
+      $query = "UPDATE list SET item = $1, category = $2, price = $3, quantity = $4, unit = $5, in_refrigerator = $6 WHERE id = $7";
+
+      $query_params = array($updated_item->item, $updated_item->category, $updated_item->price, $updated_item->quantity, $updated_item->unit, $updated_item->in_refrigerator, $updated_item->id);
+
+      $result = pg_query_params($query, $query_params);
+
+      return self::all();
+    }
+
+    static function delete($id){
+      $query = "DELETE FROM list WHERE id = $1";
+      $query_params = array($id);
+      $result = pg_query_params($query, $query_params);
+
+      return self::all();
+    }
+
   }
 
 
