75e4442fd21db0bc88a50f686ed9440b51593da0
Justin Smith <justinsmith@Justins-MacBook-Pro.local>
2019-03-14 15:31:58 -0400

    added cors - wish it was coors...

--- controllers/list.php
+++ controllers/list.php
@@ -1,6 +1,6 @@
 <?php
-
   include_once __DIR__ . '/../models/list_item.php';
+  header("Access-Control-Allow-Origin: *");
   header('Content-Type: application/json');
 
   if($_REQUEST['action'] === 'index'){
--- controllers/refrigerator.php
+++ controllers/refrigerator.php
@@ -1,5 +1,6 @@
 <?php
   include_once __DIR__ . '/../models/refrigerator_item.php';
+  header("Access-Control-Allow-Origin: *");
   header('Content-Type: application/json');
 
 
